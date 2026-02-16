3c205ebb8f132fb6e568051c70c7467ca5e789d9
RMTorrabadella04 <raul.martin.torrabadella@gmail.com>
2026-02-16 19:57:08 +0100

    Prueba datos 4

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ if not hasattr(paramiko, 'DSSKey'):
 from sshtunnel import SSHTunnelForwarder
 import pymysql
 import polars as pl
+from datetime import datetime
+
 
 def conexionBBDD_SSH():
     VM_IP = '79.112.2.0' 
@@ -54,11 +56,45 @@ def conexionBBDD_SSH():
         
         return dfs
 
+def verificarDatos(dfs):
+    for nombre, df in dfs.items():
+        print(f"\n--- {nombre} ---")
+        print(df.head())
+        print(f"Columnas: {df.columns}")
+        print(f"Tipos de datos: {df.dtypes}")
+        print(f"Valores nulos: \n{df.null_count().sum()}\n")
+        print(f"Total filas: {len(df)}")
+        
+        # Cambiaremos el formato de fecha si existe la columna 'fecha'
+        if 'Fecha' in df.columns:
+            df = df.with_columns(
+                pl.col('Fecha').map_elements(
+                    lambda x: milisegundos_a_fecha(x), 
+                    return_dtype=pl.Datetime
+                ).alias('Fecha')
+            )
+        print(df.head())
+
+
+
+def milisegundos_a_fecha(ms, tz=None):
+    ms = int(ms)
+    # Validación de entrada
+    if not isinstance(ms, (int, float)):
+        raise ValueError("El valor debe ser un número (int o float).")
+    
+    try:
+        # Convertir milisegundos a segundos
+        segundos = ms / 1000.0
+        return datetime.fromtimestamp(segundos, tz=tz)
+    except (OverflowError, OSError) as e:
+        raise ValueError(f"El valor de milisegundos no es válido: {e}")
+
+
 if __name__ == "__main__":
     datos = conexionBBDD_SSH()
     
-
-    df_ipc = datos['ipc']
-    df_datos_ipc = datos['datos_ipc']
-    df_ipv = datos['ipv']
-    df_datos_ipv = datos['datos_ipv']
+                            # Verificando podemos ver que no tenemos datos nulos, 
+    verificarDatos(datos)   # además de qestar bien formateados y estructurados ha excepción de las fechas
+                            # para hacer unos gráficos con plotly 
+    
